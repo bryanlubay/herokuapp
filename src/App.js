d6c64a7742534e5cfaba80ac3667ecb19d9eda89
lubayb1 <bryanlubay1@gmail.com>
2020-08-29 22:47:01 -0700

    29 August

--- src/App.js
+++ src/App.js
@@ -461,7 +461,7 @@ function App() {
       </Alert>
 
       <Alert className="alerts" id="frequently_asked_questions" variant="primary" onClose={() => hide_frequently_asked_questions()} dismissible hidden="true">
-        <Alert.Heading className="alert-headings">Frequently Asked Questions</Alert.Heading><br></br>
+        <Alert.Heading className="alert-headings">Frequently Asked Questions</Alert.Heading>
         <p>
 
       {/* <Table>
@@ -478,7 +478,7 @@ function App() {
       </Table> */}
 
         <Table className="table">
-          <br></br><a id="Top"><thead className="alert-headings">On This Page</thead></a><br></br><br></br>
+          <a id="Top"><thead className="alert-headings">On This Page</thead></a><br></br>
           <tbody>
             <tr className="table-row">
             <a href="#Water"><td className="tdleft" >Water</td></a>
@@ -487,14 +487,15 @@ function App() {
             </tr>
             <tr className="table-row">
             <a href="#Basics"><td className="tdleft" >Basics</td></a>
+            <a href="#Spread"><td className="tdright" >Spread</td></a>
 
-              <a href="#Funerals"><td className="tdright" >Funerals</td></a>
 
 
             </tr>
             <tr className="table-row">
             <a href="#Children"><td className="tdleft" >Children</td></a>
-            <a href="#Spread"><td className="tdright" >Spread</td></a>
+            <a href="#Funerals"><td className="tdright" >Funerals</td></a>
+
 
             </tr>
             <tr className="table-row">
