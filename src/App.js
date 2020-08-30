3f447c273ee9ee574f80ff78092191fc3511e8af
lubayb1 <bryanlubay1@gmail.com>
2020-08-29 17:34:36 -0700

    29 August

--- src/App.js
+++ src/App.js
@@ -480,7 +480,7 @@ function App() {
         <Table className="table">
           <a id="Top"><thead className="alert-headings">On This Page</thead></a>
           <tbody>
-            <tr>
+            <tr className="table-row">
               <a href="#Basics"><td >Basics</td></a>
               <a href="#People-at-Higher-Risk-for-Severe-Illness"><td >People at Higher Risk for Severe Illness</td></a>
               <a href="#Spread"><td >Spread</td></a>
