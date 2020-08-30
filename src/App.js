0747cb8141a65c685822b7e3c5641240b0a92635
lubayb1 <bryanlubay1@gmail.com>
2020-08-29 22:21:46 -0700

    29 August

--- src/App.js
+++ src/App.js
@@ -490,7 +490,7 @@ function App() {
 
             </tr>
             <tr className="table-row">
-            <a href="#People-at-Higher-Risk-for-Severe-Illness"><td className="tdleft">People at Higher Risk for Severe Illness</td></a>
+            <a href="#Community-Mitigation"><td className="tdleft">Community Mitigation</td></a>
             <a href="#Prevention"><td className="tdright">Prevention</td></a>
 
             </tr>
@@ -505,17 +505,20 @@ function App() {
             </tr>
 
             <tr className="table-row">
-            <a href="#Community-Mitigation"><td className="tdleft">Community Mitigation</td></a>
-              <a href="#Symptoms-and-Emergency-Warning-Signs"><td  className="tdright">Symptoms and Emergency Warning Signs</td></a>
-
-            </tr>
-            <tr className="table-row">
               <a href="#Preparing-for-an-Outbreak"><td className="tdleft" >Preparing for an Outbreak</td></a>
               <a href="#Testing"><td className="tdright">Testing</td></a>
 
             </tr>
             <tr className="table-row">
-            <a href="#If-You-or-Someone-You-Know-is-Sick-or-Had-Contact-with-Someone-who-Has-COVID-19"><td className="tdleft" >If You or Someone You Know is Sick or Had Contact with Someone who Has COVID-19 </td></a>
+            <a href="#If-You-or-Someone-You-Know-is-Sick-or-Had-Contact-with-Someone-who-Has-COVID-19"><td className="tdcenter" >If You or Someone You Know is Sick or Had Contact with Someone who Has COVID-19 </td></a>
+            </tr>
+
+            <tr className="table-row">
+            <a href="#People-at-Higher-Risk-for-Severe-Illness"><td className="tdcenter">People at Higher Risk for Severe Illness</td></a>
+            </tr>
+
+            <tr className="table-row">
+            <a href="#Symptoms-and-Emergency-Warning-Signs"><td  className="tdright">Symptoms and Emergency Warning Signs</td></a>
 
             </tr>
           </tbody>
