73648af8879514508e66552fb9b7af6a6adc10bb
lubayb1 <bryanlubay1@gmail.com>
2020-09-24 06:02:08 -0700

    24 September

--- src/App.js
+++ src/App.js
@@ -2093,7 +2093,7 @@ function App() {
           <Alert className="alerts" id="symptoms" variant="warning" onClose={() => hide_symptoms()} dismissible hidden="true">
             <Alert.Heading>Symptoms</Alert.Heading>
             <div data-cdc-widget='healthBot' data-cdc-theme='theme1' class='cdc-widget-color-blue'  data-cdc-language='en-us'></div>
-{/* <script src='https://t.cdc.gov/1M1B'></script> */}
+          <script src='https://t.cdc.gov/1M1B'></script>
         hello
           </Alert>
 
