6f0324aa74e0f771f9440e4216a2834b823d6a51
Bryan Lubay <BLubay@ArsenalInc.local>
2020-11-13 14:35:28 -0800

    13 November

--- src/App.js
+++ src/App.js
@@ -2380,7 +2380,7 @@ Don’t have elastic? Use hair ties or elastic head bands. If you only have stri
             </iframe> */}
 
 
-<iframe id="frame" frameborder="0" width="100%" height="100%" src="https://tools.cdc.gov/medialibrary/app/detail_widget/options_templates/template_405848_preview.html?class=cdc-widget-color-white&theme=theme1&language=en">
+<iframe id="frame" frameborder="0" width="100%" height="150%" src="https://tools.cdc.gov/medialibrary/app/detail_widget/options_templates/template_405848_preview.html?class=cdc-widget-color-white&theme=theme1&language=en">
 
 {/* <div data-cdc-widget='healthBot' data-cdc-theme='theme1' class='cdc-widget-color-white'  data-cdc-language='en-us'></div>
 <script src='https://t.cdc.gov/1M1B'></script> */}
