0983a7417eacb65fcf453d8aeb1adc2d9fd9b55f
lubayb1 <bryanlubay1@gmail.com>
2020-12-01 21:26:49 -0800

    1 Dec House

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Card, Form, Button, Alert, Accordion, Table, Image } from 'react-bootst
 // import { HashLink as Link } from 'react-router-hash-link';
 import 'react-widgets/dist/css/react-widgets.css';
 // import Iframe from 'react-iframe'
+
 import { Chart } from 'react-charts'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -326,6 +327,43 @@ function hide_symptoms() {
 }
 
 function App() {
+
+
+  const data = React.useMemo(
+    () => [
+      {
+        label: 'Series 1',
+        data: [[0, 1], [1, 2], [2, 4], [3, 2], [4, 7]]
+      },
+      {
+        label: 'Series 2',
+        data: [[0, 3], [1, 1], [2, 5], [3, 6], [4, 4]]
+      }
+    ],
+    []
+  )
+ 
+  const axes = React.useMemo(
+    () => [
+      { primary: true, type: 'linear', position: 'bottom' },
+      { type: 'linear', position: 'left' }
+    ],
+    []
+  )
+ 
+  const lineChart = (
+    // A react-chart hyper-responsively and continuously fills the available
+    // space of its parent element automatically
+    <div
+      style={{
+        width: '400px',
+        height: '300px'
+      }}
+    >
+      <Chart data={data} axes={axes} />
+    </div>
+  )
+
 
   useEffect(() => {
     get_data('nv')
