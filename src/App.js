7b4a292fe63107aaafe148128518a09531f411c1
Bryan Lubay <BLubay@ArsenalInc.local>
2020-12-03 09:12:35 -0800

    3 Dec Ars

--- src/App.js
+++ src/App.js
@@ -413,8 +413,10 @@ function App() {
 
         <Card className="card-chart" border="secondary" bg="light" text="dark">
           <Card.Header>Work in progress :)</Card.Header>
+          <div>
           <p>y axis </p>
           {lineChart}
+          </div>
           x axis
           <Card.Footer>STATE is on an UPWARD/DOWNWARD trend</Card.Footer>
         </Card>
