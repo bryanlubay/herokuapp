badddc7d1bb7e95578d0668a40a0b04822044178
Bryan Lubay <BLubay@ArsenalInc.local>
2020-12-07 15:06:46 -0800

    7 Dec Ars

--- src/App.js
+++ src/App.js
@@ -474,7 +474,13 @@ function App() {
       <header className="App-header">
         <h3 id="loading">Loading . . .</h3>
         {/* STATE SEARCH */}
-        <Form id="formStateInput" className="state-form" onSubmit={e => { update_data(); e.preventDefault(); refresh_linechart(); }}>
+        <Form id="formStateInput" className="state-form" onSubmit={e => { update_data(); e.preventDefault(); 
+        
+        useEffect(() => {
+          chart_data
+        }, [])
+
+        }}>
           <Form.Group controlId="formInput">
             <div class="form-inline">
               <Form.Label className="enter-state">Enter State </Form.Label>
