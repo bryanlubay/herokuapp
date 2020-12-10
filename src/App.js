1d8d173e03eef24175360feb8f03e60470551678
Bryan Lubay <BLubay@ArsenalInc.local>
2020-12-10 12:40:55 -0800

    10 Dec Ars

--- src/App.js
+++ src/App.js
@@ -282,7 +282,7 @@ function hide_symptoms() {
   document.getElementById("symptoms").hidden = true
 }
 
-let days = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+let days = [29,30,31,32,33,34,35,36,37,38,39,40,41,42]
 let infected = [15,16,17,18,19,20,21,22,23,24,25,26,27,28]
 let deaths = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
 const get_data = async (state = 'nv') => {
