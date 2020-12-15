eadc32a7e4cbeb5b31f0e718f7302d2fe96aa316
Bryan Lubay <BLubay@ArsenalInc.local>
2020-12-15 13:41:46 -0800

    15 Dec Ars

--- src/App.js
+++ src/App.js
@@ -213,8 +213,8 @@ function convertState(state) {
 
 function convertEpoch(epoch) {
   var d = new Date(0);
+  d.setUTCSeconds(epoch)
   // d.setUTCDate(epoch)
-  d.setUTCDate(epoch)
   return d.getDate()
 }
 
