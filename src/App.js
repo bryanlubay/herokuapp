6c68433d507058fd6560451bf1d48de026c7c631
Bryan Lubay <BLubay@ArsenalInc.local>
2020-12-15 09:40:34 -0800

    15 Dec Ars

--- src/App.js
+++ src/App.js
@@ -328,6 +328,47 @@ function update_data() {
   get_data(temp)
 } 
 
+function update_deaths() {
+
+  return [
+    [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates1")*/, localStorage.getItem("deaths1")    ],   
+    [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates2")*/, localStorage.getItem("deaths2")    ],   
+    [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates3")*/, localStorage.getItem("deaths3")    ],   
+    [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates4")*/, localStorage.getItem("deaths4")    ],   
+    [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates5")*/, localStorage.getItem("deaths5")    ],   
+    [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates6")*/, localStorage.getItem("deaths6")    ],   
+    [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates7")*/, localStorage.getItem("deaths7")    ],   
+    [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates8")*/, localStorage.getItem("deaths8")    ],   
+    [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates9")*/, localStorage.getItem("deaths9")    ],   
+    [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates10")*/, localStorage.getItem("deaths10")   ],   
+    [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates11")*/, localStorage.getItem("deaths11")   ],   
+    [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates12")*/, localStorage.getItem("deaths12")   ],   
+    [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates13")*/, localStorage.getItem("deaths13")   ],   
+    [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates14")*/, localStorage.getItem("deaths14")   ]   
+  ]
+}
+
+function update_positives() {
+
+  return [
+    [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates1")*/, localStorage.getItem("positives1")    ],   
+    [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates2")*/, localStorage.getItem("positives2")    ],   
+    [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates3")*/, localStorage.getItem("positives3")    ],   
+    [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates4")*/, localStorage.getItem("positives4")    ],   
+    [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates5")*/, localStorage.getItem("positives5")    ],   
+    [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates6")*/, localStorage.getItem("positives6")    ],   
+    [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates7")*/, localStorage.getItem("positives7")    ],   
+    [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates8")*/, localStorage.getItem("positives8")    ],   
+    [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates9")*/, localStorage.getItem("positives9")    ],   
+    [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates10")*/, localStorage.getItem("positives10")   ],   
+    [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates11")*/, localStorage.getItem("positives11")   ],   
+    [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates12")*/, localStorage.getItem("positives12")   ],   
+    [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates13")*/, localStorage.getItem("positives13")   ],   
+    [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates14")*/, localStorage.getItem("positives14")   ]   
+  ]
+}
+
+
 /************************************************************************/
 /************************************************************************/
 /************************************************************************/
@@ -405,41 +446,49 @@ function useChartConfig() {
      [
       {
         label: 'Deaths',
-        data: [
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates1")*/, localStorage.getItem("deaths1")    ],   
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates2")*/, localStorage.getItem("deaths2")    ],   
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates3")*/, localStorage.getItem("deaths3")    ],   
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates4")*/, localStorage.getItem("deaths4")    ],   
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates5")*/, localStorage.getItem("deaths5")    ],   
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates6")*/, localStorage.getItem("deaths6")    ],   
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates7")*/, localStorage.getItem("deaths7")    ],   
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates8")*/, localStorage.getItem("deaths8")    ],   
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates9")*/, localStorage.getItem("deaths9")    ],   
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates10")*/, localStorage.getItem("deaths10")   ],   
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates11")*/, localStorage.getItem("deaths11")   ],   
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates12")*/, localStorage.getItem("deaths12")   ],   
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates13")*/, localStorage.getItem("deaths13")   ],   
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates14")*/, localStorage.getItem("deaths14")   ]   
-        ]
+        data: update_deaths()
+        
+        
+        // [
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates1")*/, localStorage.getItem("deaths1")    ],   
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates2")*/, localStorage.getItem("deaths2")    ],   
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates3")*/, localStorage.getItem("deaths3")    ],   
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates4")*/, localStorage.getItem("deaths4")    ],   
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates5")*/, localStorage.getItem("deaths5")    ],   
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates6")*/, localStorage.getItem("deaths6")    ],   
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates7")*/, localStorage.getItem("deaths7")    ],   
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates8")*/, localStorage.getItem("deaths8")    ],   
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates9")*/, localStorage.getItem("deaths9")    ],   
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates10")*/, localStorage.getItem("deaths10")   ],   
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates11")*/, localStorage.getItem("deaths11")   ],   
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates12")*/, localStorage.getItem("deaths12")   ],   
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates13")*/, localStorage.getItem("deaths13")   ],   
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates14")*/, localStorage.getItem("deaths14")   ]   
+        // ]
+
       },
       {
-        label: 'Infected',
-        data: [
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates1")*/, localStorage.getItem("positives1")],
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates2")*/, localStorage.getItem("positives2")],
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates3")*/, localStorage.getItem("positives3")],
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates4")*/, localStorage.getItem("positives4")],
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates5")*/, localStorage.getItem("positives5")],
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates6")*/, localStorage.getItem("positives6")],
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates7")*/, localStorage.getItem("positives7")],
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates8")*/, localStorage.getItem("positives8")],
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates9")*/, localStorage.getItem("positives9")],
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates10")*/, localStorage.getItem("positives10")],
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates11")*/, localStorage.getItem("positives11")],
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates12")*/, localStorage.getItem("positives12")],
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates13")*/, localStorage.getItem("positives13")],
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates14")*/, localStorage.getItem("positives14")]
-        ]
+        label: 'Positives',
+        data: update_positives()
+        
+        // [
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates1")*/, localStorage.getItem("positives1")],
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates2")*/, localStorage.getItem("positives2")],
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates3")*/, localStorage.getItem("positives3")],
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates4")*/, localStorage.getItem("positives4")],
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates5")*/, localStorage.getItem("positives5")],
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates6")*/, localStorage.getItem("positives6")],
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates7")*/, localStorage.getItem("positives7")],
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates8")*/, localStorage.getItem("positives8")],
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates9")*/, localStorage.getItem("positives9")],
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates10")*/, localStorage.getItem("positives10")],
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates11")*/, localStorage.getItem("positives11")],
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates12")*/, localStorage.getItem("positives12")],
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates13")*/, localStorage.getItem("positives13")],
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates14")*/, localStorage.getItem("positives14")]
+        // ]
+
+
       }
     ],
     []
@@ -450,41 +499,50 @@ function useChartConfig() {
      [
       {
         label: 'Deaths',
-        data: [
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates1")*/, localStorage.getItem("deaths1")    ],   
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates2")*/, localStorage.getItem("deaths2")    ],   
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates3")*/, localStorage.getItem("deaths3")    ],   
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates4")*/, localStorage.getItem("deaths4")    ],   
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates5")*/, localStorage.getItem("deaths5")    ],   
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates6")*/, localStorage.getItem("deaths6")    ],   
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates7")*/, localStorage.getItem("deaths7")    ],   
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates8")*/, localStorage.getItem("deaths8")    ],   
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates9")*/, localStorage.getItem("deaths9")    ],   
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates10")*/, localStorage.getItem("deaths10")   ],   
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates11")*/, localStorage.getItem("deaths11")   ],   
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates12")*/, localStorage.getItem("deaths12")   ],   
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates13")*/, localStorage.getItem("deaths13")   ],   
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates14")*/, localStorage.getItem("deaths14")   ]   
-        ]
+        data: update_deaths()
+        
+        
+        // [
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates1")*/, localStorage.getItem("deaths1")    ],   
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates2")*/, localStorage.getItem("deaths2")    ],   
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates3")*/, localStorage.getItem("deaths3")    ],   
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates4")*/, localStorage.getItem("deaths4")    ],   
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates5")*/, localStorage.getItem("deaths5")    ],   
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates6")*/, localStorage.getItem("deaths6")    ],   
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates7")*/, localStorage.getItem("deaths7")    ],   
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates8")*/, localStorage.getItem("deaths8")    ],   
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates9")*/, localStorage.getItem("deaths9")    ],   
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates10")*/, localStorage.getItem("deaths10")   ],   
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates11")*/, localStorage.getItem("deaths11")   ],   
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates12")*/, localStorage.getItem("deaths12")   ],   
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates13")*/, localStorage.getItem("deaths13")   ],   
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates14")*/, localStorage.getItem("deaths14")   ]   
+        // ]
+
+
       },
       {
-        label: 'Infected',
-        data: [
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates1")*/, localStorage.getItem("positives1")],
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates2")*/, localStorage.getItem("positives2")],
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates3")*/, localStorage.getItem("positives3")],
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates4")*/, localStorage.getItem("positives4")],
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates5")*/, localStorage.getItem("positives5")],
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates6")*/, localStorage.getItem("positives6")],
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates7")*/, localStorage.getItem("positives7")],
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates8")*/, localStorage.getItem("positives8")],
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates9")*/, localStorage.getItem("positives9")],
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates10")*/, localStorage.getItem("positives10")],
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates11")*/, localStorage.getItem("positives11")],
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates12")*/, localStorage.getItem("positives12")],
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates13")*/, localStorage.getItem("positives13")],
-          [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates14")*/, localStorage.getItem("positives14")]
-        ]
+        label: 'Positives',
+        data: update_positives()
+        
+        // [
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates1")*/, localStorage.getItem("positives1")],
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates2")*/, localStorage.getItem("positives2")],
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates3")*/, localStorage.getItem("positives3")],
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates4")*/, localStorage.getItem("positives4")],
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates5")*/, localStorage.getItem("positives5")],
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates6")*/, localStorage.getItem("positives6")],
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates7")*/, localStorage.getItem("positives7")],
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates8")*/, localStorage.getItem("positives8")],
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates9")*/, localStorage.getItem("positives9")],
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates10")*/, localStorage.getItem("positives10")],
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates11")*/, localStorage.getItem("positives11")],
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates12")*/, localStorage.getItem("positives12")],
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates13")*/, localStorage.getItem("positives13")],
+        //   [  Math.floor(Math.random() * 100) + 1  /*localStorage.getItem("dates14")*/, localStorage.getItem("positives14")]
+        // ]
+
+
       }
     ],
     []
