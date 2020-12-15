d60916d7fb7424d250913cfaef49c5bebd2dd948
Bryan Lubay <BLubay@ArsenalInc.local>
2020-12-15 15:07:22 -0800

    15 Dec Ars

--- src/App.js
+++ src/App.js
@@ -319,8 +319,6 @@ const get_data = async (state = 'nv') => {
   document.getElementById("loading").hidden = true
   document.getElementById("formStateInput").hidden = false
 
-  useChartConfig()
-
   return data
 }; // End get_data
 
