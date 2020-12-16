02e51c21b2016025b32f56883ea90d7005116655
Bryan Lubay <BLubay@ArsenalInc.local>
2020-12-16 10:34:24 -0800

    16 Dec Ars

--- src/App.js
+++ src/App.js
@@ -385,7 +385,7 @@ function update_positives() {
 function useChartConfig() { // happens before get_data I think, fix order to fix needing to click submit twice to change graph
 
   // let temp = convertState(document.getElementById('input').value)
-  get_data(temp)
+  get_data()
 
 
   const get_chart_data = async (state = 'nv') => {
