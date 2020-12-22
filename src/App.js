40de357bc1ab6a8069034224116a9941c7920561
Bryan Lubay <BLubay@ArsenalInc.local>
2020-12-22 10:24:25 -0800

    22 Dec Ars

--- src/App.js
+++ src/App.js
@@ -334,60 +334,67 @@ const get_data = async (state = 'nv') => {
 let deathstemp = [] // [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]  // [15000,14000,13000,12000,11000,10000,9000,8000,7000,6000,5000,4000,3000,2000,1000]
 let positivestemp = [] // [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]  // [1500,1400,1300,1200,1100,1000,900,800,700,600,500,400,300,200,100]
 let datestemp = [] // [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]  // [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15] 
-
+let initialize = 1;
 function useChartConfig() { // happens before get_data I think, fix order to fix needing to click submit twice to change graph
 
-  get_data().then(function(data) {
-    document.getElementById('chart-header').textContent = deathstemp
-    deathstemp[0] = data.Deaths[data.Deaths.length - 1] // most recent
-    deathstemp[1] = data.Deaths[data.Deaths.length - 2]
-    deathstemp[2] = data.Deaths[data.Deaths.length - 3]
-    deathstemp[3] = data.Deaths[data.Deaths.length - 4]
-    deathstemp[4] = data.Deaths[data.Deaths.length - 5]
-    deathstemp[5] = data.Deaths[data.Deaths.length - 6]
-    deathstemp[6] = data.Deaths[data.Deaths.length - 7]
-    deathstemp[7] = data.Deaths[data.Deaths.length - 8]
-    deathstemp[8] = data.Deaths[data.Deaths.length - 9]
-    deathstemp[9] = data.Deaths[data.Deaths.length - 10]
-    deathstemp[10] = data.Deaths[data.Deaths.length - 11]
-    deathstemp[11] = data.Deaths[data.Deaths.length - 12]
-    deathstemp[12] = data.Deaths[data.Deaths.length - 13]
-    deathstemp[13] = data.Deaths[data.Deaths.length - 14]
-    deathstemp[14] = data.Deaths[data.Deaths.length - 15]
-
-    positivestemp[0] = data.Positive[data.Positive.length - 1] // most recent
-    positivestemp[1] = data.Positive[data.Positive.length - 2]
-    positivestemp[2] = data.Positive[data.Positive.length - 3]
-    positivestemp[3] = data.Positive[data.Positive.length - 4]
-    positivestemp[4] = data.Positive[data.Positive.length - 5]
-    positivestemp[5] = data.Positive[data.Positive.length - 6]
-    positivestemp[6] = data.Positive[data.Positive.length - 7]
-    positivestemp[7] = data.Positive[data.Positive.length - 8]
-    positivestemp[8] = data.Positive[data.Positive.length - 9]
-    positivestemp[9] = data.Positive[data.Positive.length - 10]
-    positivestemp[10] = data.Positive[data.Positive.length - 11]
-    positivestemp[11] = data.Positive[data.Positive.length - 12]
-    positivestemp[12] = data.Positive[data.Positive.length - 13]
-    positivestemp[13] = data.Positive[data.Positive.length - 14]
-    positivestemp[14] = data.Positive[data.Positive.length - 15]
-
-    datestemp[0] = convertEpoch(data.Date[data.Date.length - 1] )
-    datestemp[1] = convertEpoch(data.Date[data.Date.length - 2])
-    datestemp[2] = convertEpoch(data.Date[data.Date.length - 3])
-    datestemp[3] = convertEpoch(data.Date[data.Date.length - 4])
-    datestemp[4] = convertEpoch(data.Date[data.Date.length - 5])
-    datestemp[5] = convertEpoch(data.Date[data.Date.length - 6])
-    datestemp[6] = convertEpoch(data.Date[data.Date.length - 7])
-    datestemp[7] = convertEpoch(data.Date[data.Date.length - 8])
-    datestemp[8] = convertEpoch(data.Date[data.Date.length - 9])
-    datestemp[9] = convertEpoch(data.Date[data.Date.length - 10])
-    datestemp[10] = convertEpoch(data.Date[data.Date.length - 11])
-    datestemp[11] = convertEpoch(data.Date[data.Date.length - 12])
-    datestemp[12] = convertEpoch(data.Date[data.Date.length - 13])
-    datestemp[13] = convertEpoch(data.Date[data.Date.length - 14])
-    datestemp[14] = convertEpoch(data.Date[data.Date.length - 15])
-    // document.getElementById('chart-header').textContent = deathstemp
-  })
+  // 
+  if (initialize == 1){
+    initialize = 0;
+  }
+  else {
+    get_data().then(function(data) {
+      document.getElementById('chart-header').textContent = deathstemp
+      deathstemp[0] = data.Deaths[data.Deaths.length - 1] // most recent
+      deathstemp[1] = data.Deaths[data.Deaths.length - 2]
+      deathstemp[2] = data.Deaths[data.Deaths.length - 3]
+      deathstemp[3] = data.Deaths[data.Deaths.length - 4]
+      deathstemp[4] = data.Deaths[data.Deaths.length - 5]
+      deathstemp[5] = data.Deaths[data.Deaths.length - 6]
+      deathstemp[6] = data.Deaths[data.Deaths.length - 7]
+      deathstemp[7] = data.Deaths[data.Deaths.length - 8]
+      deathstemp[8] = data.Deaths[data.Deaths.length - 9]
+      deathstemp[9] = data.Deaths[data.Deaths.length - 10]
+      deathstemp[10] = data.Deaths[data.Deaths.length - 11]
+      deathstemp[11] = data.Deaths[data.Deaths.length - 12]
+      deathstemp[12] = data.Deaths[data.Deaths.length - 13]
+      deathstemp[13] = data.Deaths[data.Deaths.length - 14]
+      deathstemp[14] = data.Deaths[data.Deaths.length - 15]
+  
+      positivestemp[0] = data.Positive[data.Positive.length - 1] // most recent
+      positivestemp[1] = data.Positive[data.Positive.length - 2]
+      positivestemp[2] = data.Positive[data.Positive.length - 3]
+      positivestemp[3] = data.Positive[data.Positive.length - 4]
+      positivestemp[4] = data.Positive[data.Positive.length - 5]
+      positivestemp[5] = data.Positive[data.Positive.length - 6]
+      positivestemp[6] = data.Positive[data.Positive.length - 7]
+      positivestemp[7] = data.Positive[data.Positive.length - 8]
+      positivestemp[8] = data.Positive[data.Positive.length - 9]
+      positivestemp[9] = data.Positive[data.Positive.length - 10]
+      positivestemp[10] = data.Positive[data.Positive.length - 11]
+      positivestemp[11] = data.Positive[data.Positive.length - 12]
+      positivestemp[12] = data.Positive[data.Positive.length - 13]
+      positivestemp[13] = data.Positive[data.Positive.length - 14]
+      positivestemp[14] = data.Positive[data.Positive.length - 15]
+  
+      datestemp[0] = convertEpoch(data.Date[data.Date.length - 1] )
+      datestemp[1] = convertEpoch(data.Date[data.Date.length - 2])
+      datestemp[2] = convertEpoch(data.Date[data.Date.length - 3])
+      datestemp[3] = convertEpoch(data.Date[data.Date.length - 4])
+      datestemp[4] = convertEpoch(data.Date[data.Date.length - 5])
+      datestemp[5] = convertEpoch(data.Date[data.Date.length - 6])
+      datestemp[6] = convertEpoch(data.Date[data.Date.length - 7])
+      datestemp[7] = convertEpoch(data.Date[data.Date.length - 8])
+      datestemp[8] = convertEpoch(data.Date[data.Date.length - 9])
+      datestemp[9] = convertEpoch(data.Date[data.Date.length - 10])
+      datestemp[10] = convertEpoch(data.Date[data.Date.length - 11])
+      datestemp[11] = convertEpoch(data.Date[data.Date.length - 12])
+      datestemp[12] = convertEpoch(data.Date[data.Date.length - 13])
+      datestemp[13] = convertEpoch(data.Date[data.Date.length - 14])
+      datestemp[14] = convertEpoch(data.Date[data.Date.length - 15])
+      // document.getElementById('chart-header').textContent = deathstemp
+    })
+  
+  }
 
   // get_data().then(function(data) {})
 
@@ -534,113 +541,11 @@ function App() {
 
   document.title = "Bryan Lubay's App :)"
 
-
-  // const [state, setState] = React.useState(
-  //   // get_data().then(function(data) {}),
-  //   { 
-  //   data: [
-  //     {
-  //       label: 'Deaths',
-  //       data:
-  //       [
-  //         [datestemp[0],  deathstemp[0] - deathstemp[1]],   
-  //         [datestemp[1],  deathstemp[1] - deathstemp[2]  ],   
-  //         [datestemp[2],  deathstemp[2] - deathstemp[3]  ],   
-  //         [datestemp[3],  deathstemp[3] - deathstemp[4]  ],   
-  //         [datestemp[4],  deathstemp[4] - deathstemp[5]  ],   
-  //         [datestemp[5],  deathstemp[5] - deathstemp[6]  ],   
-  //         [datestemp[6],  deathstemp[6] - deathstemp[7]  ],   
-  //         [datestemp[7],  deathstemp[7] - deathstemp[8]  ],   
-  //         [datestemp[8],  deathstemp[8] - deathstemp[9]  ],   
-  //         [datestemp[9],  deathstemp[9] - deathstemp[10]  ],   
-  //         [datestemp[10],  deathstemp[10] - deathstemp[11]  ],   
-  //         [datestemp[11],  deathstemp[11] - deathstemp[12]  ],   
-  //         [datestemp[12],  deathstemp[12] - deathstemp[13]  ],   
-  //         [datestemp[13],  deathstemp[13] - deathstemp[14]  ]   
-  //       ]},
-  //     {
-  //       label: 'Positives',
-  //       data:
-  //       [
-  //         [datestemp[0],  positivestemp[0] - positivestemp[1]],   
-  //         [datestemp[1],  positivestemp[1] - positivestemp[2]  ],   
-  //         [datestemp[2],  positivestemp[2] - positivestemp[3]  ],   
-  //         [datestemp[3],  positivestemp[3] - positivestemp[4]  ],   
-  //         [datestemp[4],  positivestemp[4] - positivestemp[5]  ],   
-  //         [datestemp[5],  positivestemp[5] - positivestemp[6]  ],   
-  //         [datestemp[6],  positivestemp[6] - positivestemp[7]  ],   
-  //         [datestemp[7],  positivestemp[7] - positivestemp[8]  ],   
-  //         [datestemp[8],  positivestemp[8] - positivestemp[9]  ],   
-  //         [datestemp[9],  positivestemp[9] - positivestemp[10]  ],   
-  //         [datestemp[10],  positivestemp[10] - positivestemp[11]  ],   
-  //         [datestemp[11],  positivestemp[11] - positivestemp[12]  ],   
-  //         [datestemp[12],  positivestemp[12] - positivestemp[13]  ],   
-  //         [datestemp[13],  positivestemp[13] - positivestemp[14]  ]   
-  //       ]}]})
-
-  // React.useEffect(() => {
-  //   setState(old => ({
-  //     ...old,
-  //     data: [
-  //     {
-  //       label: 'Deaths',
-  //       data: 
-  //       [
-  //         [datestemp[0],  deathstemp[0] - deathstemp[1]],   
-  //         [datestemp[1],  deathstemp[1] - deathstemp[2]  ],   
-  //         [datestemp[2],  deathstemp[2] - deathstemp[3]  ],   
-  //         [datestemp[3],  deathstemp[3] - deathstemp[4]  ],   
-  //         [datestemp[4],  deathstemp[4] - deathstemp[5]  ],   
-  //         [datestemp[5],  deathstemp[5] - deathstemp[6]  ],   
-  //         [datestemp[6],  deathstemp[6] - deathstemp[7]  ],   
-  //         [datestemp[7],  deathstemp[7] - deathstemp[8]  ],   
-  //         [datestemp[8],  deathstemp[8] - deathstemp[9]  ],   
-  //         [datestemp[9],  deathstemp[9] - deathstemp[10]  ],   
-  //         [datestemp[10],  deathstemp[10] - deathstemp[11]  ],   
-  //         [datestemp[11],  deathstemp[11] - deathstemp[12]  ],   
-  //         [datestemp[12],  deathstemp[12] - deathstemp[13]  ],   
-  //         [datestemp[13],  deathstemp[13] - deathstemp[14]  ]   
-  //       ]},
-  //     {
-  //       label: 'Positives',
-  //       data:
-  //       [
-  //         [datestemp[0],  positivestemp[0] - positivestemp[1]],   
-  //         [datestemp[1],  positivestemp[1] - positivestemp[2]  ],   
-  //         [datestemp[2],  positivestemp[2] - positivestemp[3]  ],   
-  //         [datestemp[3],  positivestemp[3] - positivestemp[4]  ],   
-  //         [datestemp[4],  positivestemp[4] - positivestemp[5]  ],   
-  //         [datestemp[5],  positivestemp[5] - positivestemp[6]  ],   
-  //         [datestemp[6],  positivestemp[6] - positivestemp[7]  ],   
-  //         [datestemp[7],  positivestemp[7] - positivestemp[8]  ],   
-  //         [datestemp[8],  positivestemp[8] - positivestemp[9]  ],   
-  //         [datestemp[9],  positivestemp[9] - positivestemp[10]  ],   
-  //         [datestemp[10],  positivestemp[10] - positivestemp[11]  ],   
-  //         [datestemp[11],  positivestemp[11] - positivestemp[12]  ],   
-  //         [datestemp[12],  positivestemp[12] - positivestemp[13]  ],   
-  //         [datestemp[13],  positivestemp[13] - positivestemp[14]  ]   
-  //       ]}]}))}, [])
-
-  // initialize
-  // useEffect(() => {get_data('nv')}, []) // lol make third const to initialize
-  // get_data()
-
   const series = React.useMemo(() => ({showPoints: false}),[])
 
   const axes = React.useMemo(() => [{ primary: true, type: 'linear', position: 'bottom' }, { type: 'linear', position: 'left' }],[])
 
   const { data, updateChartData } = useChartConfig() // gets called first and calls getChartData
-
-  // const { data2, updateChartData } = useChartConfig() // gets called first and calls getChartData
-
-  // function temp() {
-  //   updateChartData()
-  //   updateChartData2()
-  // }
-
-  // useChartConfig()
-
-  // useChartConfig()
 
   useEffect(()=> {
     updateChartData()
@@ -652,12 +557,6 @@ function App() {
     <div style={{ margin: 'auto', width: '80vw', height: '80vh', maxWidth: '-webkit-fill-available', maxHeight: '-webkit-fill-available' }}>
       <Chart id="chart" data={data} series={series} axes={axes} tooltip></Chart>
     </div>)
-
-// let lineChart2 = (
-//   <div style={{ margin: 'auto', width: '80vw', height: '80vh', maxWidth: '-webkit-fill-available', maxHeight: '-webkit-fill-available' }}>
-//     <Chart id="chart" data={data2} series={series} axes={axes} tooltip></Chart>
-//   </div>)
-
 
   return (
 
