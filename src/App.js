fe99c7149a9596f687074bd19f86a52772524ae8
Bryan Lubay <BLubay@ArsenalInc.local>
2020-12-23 08:58:41 -0800

    23 Dec Ars

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // heroku git:remote -a bryanlubay
 
-import React, { useCallback, useEffect } from 'react';
+import React from 'react';
 import { Card, Form, Button, Alert, Accordion, Table, Image } from 'react-bootstrap';
 import { Chart } from 'react-charts'
 
@@ -281,15 +281,6 @@ function hide_symptoms() {
 /************************************************************************/
 /************************************************************************/
 /************************************************************************/
-
-/************************************************************************/
-/************************************************************************/
-/************************************************************************/
-let deathstemp = [] // [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]  // [15000,14000,13000,12000,11000,10000,9000,8000,7000,6000,5000,4000,3000,2000,1000]
-let positivestemp = [] // [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]  // [1500,1400,1300,1200,1100,1000,900,800,700,600,500,400,300,200,100]
-let datestemp = [] // [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]  // [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15] 
-let initialize = 1;
-
 function update_chart () {
   return       [
     {
@@ -420,7 +411,6 @@ function App() {
     number = parseInt(document.getElementById('deaths').textContent = data.Deaths[data.Deaths.length - 1]) - parseInt(document.getElementById('deaths').textContent = data.Deaths[data.Deaths.length - 2])
     document.getElementById('deaths').textContent = data.Deaths[data.Deaths.length - 1] + " Deaths (+" + number + ")" + asterik
   
-  
     var d = new Date(0);
     d.setUTCSeconds(data.Date[data.Date.length - 1])
   
@@ -439,7 +429,6 @@ function App() {
     return data
   }; // End get_data
   
-
   const [state, setState] = React.useState(
     { 
     data: [
@@ -496,117 +485,19 @@ function App() {
 
       }]})
 
-  function useChartConfig() { // happens before get_data I think, fix order to fix needing to click submit twice to change graph
-      // document.getElementById('chart-header').textContent = data
+  function useChartConfig() { 
 
-    // get_data().then(function(data) { // this should be called by form submit
-    //   document.getElementById('chart-header').textContent = deathstemp
-    //   deathstemp[0] = data.Deaths[data.Deaths.length - 1] // most recent
-    //   deathstemp[1] = data.Deaths[data.Deaths.length - 2]
-    //   deathstemp[2] = data.Deaths[data.Deaths.length - 3]
-    //   deathstemp[3] = data.Deaths[data.Deaths.length - 4]
-    //   deathstemp[4] = data.Deaths[data.Deaths.length - 5]
-    //   deathstemp[5] = data.Deaths[data.Deaths.length - 6]
-    //   deathstemp[6] = data.Deaths[data.Deaths.length - 7]
-    //   deathstemp[7] = data.Deaths[data.Deaths.length - 8]
-    //   deathstemp[8] = data.Deaths[data.Deaths.length - 9]
-    //   deathstemp[9] = data.Deaths[data.Deaths.length - 10]
-    //   deathstemp[10] = data.Deaths[data.Deaths.length - 11]
-    //   deathstemp[11] = data.Deaths[data.Deaths.length - 12]
-    //   deathstemp[12] = data.Deaths[data.Deaths.length - 13]
-    //   deathstemp[13] = data.Deaths[data.Deaths.length - 14]
-    //   deathstemp[14] = data.Deaths[data.Deaths.length - 15]
-  
-    //   positivestemp[0] = data.Positive[data.Positive.length - 1] // most recent
-    //   positivestemp[1] = data.Positive[data.Positive.length - 2]
-    //   positivestemp[2] = data.Positive[data.Positive.length - 3]
-    //   positivestemp[3] = data.Positive[data.Positive.length - 4]
-    //   positivestemp[4] = data.Positive[data.Positive.length - 5]
-    //   positivestemp[5] = data.Positive[data.Positive.length - 6]
-    //   positivestemp[6] = data.Positive[data.Positive.length - 7]
-    //   positivestemp[7] = data.Positive[data.Positive.length - 8]
-    //   positivestemp[8] = data.Positive[data.Positive.length - 9]
-    //   positivestemp[9] = data.Positive[data.Positive.length - 10]
-    //   positivestemp[10] = data.Positive[data.Positive.length - 11]
-    //   positivestemp[11] = data.Positive[data.Positive.length - 12]
-    //   positivestemp[12] = data.Positive[data.Positive.length - 13]
-    //   positivestemp[13] = data.Positive[data.Positive.length - 14]
-    //   positivestemp[14] = data.Positive[data.Positive.length - 15]
-  
-    //   datestemp[0] = convertEpoch(data.Date[data.Date.length - 1] )
-    //   datestemp[1] = convertEpoch(data.Date[data.Date.length - 2])
-    //   datestemp[2] = convertEpoch(data.Date[data.Date.length - 3])
-    //   datestemp[3] = convertEpoch(data.Date[data.Date.length - 4])
-    //   datestemp[4] = convertEpoch(data.Date[data.Date.length - 5])
-    //   datestemp[5] = convertEpoch(data.Date[data.Date.length - 6])
-    //   datestemp[6] = convertEpoch(data.Date[data.Date.length - 7])
-    //   datestemp[7] = convertEpoch(data.Date[data.Date.length - 8])
-    //   datestemp[8] = convertEpoch(data.Date[data.Date.length - 9])
-    //   datestemp[9] = convertEpoch(data.Date[data.Date.length - 10])
-    //   datestemp[10] = convertEpoch(data.Date[data.Date.length - 11])
-    //   datestemp[11] = convertEpoch(data.Date[data.Date.length - 12])
-    //   datestemp[12] = convertEpoch(data.Date[data.Date.length - 13])
-    //   datestemp[13] = convertEpoch(data.Date[data.Date.length - 14])
-    //   datestemp[14] = convertEpoch(data.Date[data.Date.length - 15])
-    //   // document.getElementById('chart-header').textContent = deathstemp
-    // })
-
-
-  React.useEffect(() => { // initializes
+  React.useEffect(() => { 
     setState(old => ({
       ...old,
       data: [
       {
         label: 'Deaths',
         data: [[0,0]]
-        // update_chart2()
-        // [
-        //   [update_chart(),  update_chart()]
-        // ]
-
-        // [
-        //   [update_chart(),  update_chart()],
-        //   [datestemp[0],  deathstemp[0] - deathstemp[1]],   
-        //   [datestemp[1],  deathstemp[1] - deathstemp[2]  ],   
-        //   [datestemp[2],  deathstemp[2] - deathstemp[3]  ],   
-        //   [datestemp[3],  deathstemp[3] - deathstemp[4]  ],   
-        //   [datestemp[4],  deathstemp[4] - deathstemp[5]  ],   
-        //   [datestemp[5],  deathstemp[5] - deathstemp[6]  ],   
-        //   [datestemp[6],  deathstemp[6] - deathstemp[7]  ],   
-        //   [datestemp[7],  deathstemp[7] - deathstemp[8]  ],   
-        //   [datestemp[8],  deathstemp[8] - deathstemp[9]  ],   
-        //   [datestemp[9],  deathstemp[9] - deathstemp[10]  ],   
-        //   [datestemp[10],  deathstemp[10] - deathstemp[11]  ],   
-        //   [datestemp[11],  deathstemp[11] - deathstemp[12]  ],   
-        //   [datestemp[12],  deathstemp[12] - deathstemp[13]  ],   
-        //   [datestemp[13],  deathstemp[13] - deathstemp[14]  ]   
-        // ]
       },
       {
         label: 'Positives',
         data: [[0,0]]
-        // update_chart2()
-        // [
-        //   [update_chart(),  update_chart()]
-        // ]
-
-        // [
-        //   [update_chart(),  update_chart()],
-        //   [datestemp[0],  positivestemp[0] - positivestemp[1]],   
-        //   [datestemp[1],  positivestemp[1] - positivestemp[2]  ],   
-        //   [datestemp[2],  positivestemp[2] - positivestemp[3]  ],   
-        //   [datestemp[3],  positivestemp[3] - positivestemp[4]  ],   
-        //   [datestemp[4],  positivestemp[4] - positivestemp[5]  ],   
-        //   [datestemp[5],  positivestemp[5] - positivestemp[6]  ],   
-        //   [datestemp[6],  positivestemp[6] - positivestemp[7]  ],   
-        //   [datestemp[7],  positivestemp[7] - positivestemp[8]  ],   
-        //   [datestemp[8],  positivestemp[8] - positivestemp[9]  ],   
-        //   [datestemp[9],  positivestemp[9] - positivestemp[10]  ],   
-        //   [datestemp[10],  positivestemp[10] - positivestemp[11]  ],   
-        //   [datestemp[11],  positivestemp[11] - positivestemp[12]  ],   
-        //   [datestemp[12],  positivestemp[12] - positivestemp[13]  ],   
-        //   [datestemp[13],  positivestemp[13] - positivestemp[14]  ]   
-        // ]
       }]}))}, [])
 
     
@@ -614,51 +505,7 @@ function App() {
     setState(old => ({
       ...old,
       data: update_chart()
-      // [
-      // {
-      //   label: 'Deaths',
-      //   data: 
-      //   [
-      //     // [update_chart(),  update_chart()],
-      //     [sessionStorage.getItem("dates1"),  sessionStorage.getItem("deaths1") - sessionStorage.getItem("deaths2")],   
-      //     [sessionStorage.getItem("dates2"),  sessionStorage.getItem("deaths2") - sessionStorage.getItem("deaths3")],   
-      //     [sessionStorage.getItem("dates3"),  sessionStorage.getItem("deaths3") - sessionStorage.getItem("deaths4")],   
-      //     [sessionStorage.getItem("dates4"),  sessionStorage.getItem("deaths4") - sessionStorage.getItem("deaths5")],   
-      //     [sessionStorage.getItem("dates5"),  sessionStorage.getItem("deaths5") - sessionStorage.getItem("deaths6")],   
-      //     [sessionStorage.getItem("dates6"),  sessionStorage.getItem("deaths6") - sessionStorage.getItem("deaths7")],   
-      //     [sessionStorage.getItem("dates7"),  sessionStorage.getItem("deaths7") - sessionStorage.getItem("deaths8")],   
-      //     [sessionStorage.getItem("dates8"),  sessionStorage.getItem("deaths8") - sessionStorage.getItem("deaths9")],   
-      //     [sessionStorage.getItem("dates9"),  sessionStorage.getItem("deaths9") - sessionStorage.getItem("deaths10")],   
-      //     [sessionStorage.getItem("dates10"),  sessionStorage.getItem("deaths10") - sessionStorage.getItem("deaths11")],   
-      //     [sessionStorage.getItem("dates11"),  sessionStorage.getItem("deaths11") - sessionStorage.getItem("deaths12")],   
-      //     [sessionStorage.getItem("dates12"),  sessionStorage.getItem("deaths12") - sessionStorage.getItem("deaths13")],   
-      //     [sessionStorage.getItem("dates13"),  sessionStorage.getItem("deaths13") - sessionStorage.getItem("deaths14")],   
-      //     [sessionStorage.getItem("dates14"),  sessionStorage.getItem("deaths14") - sessionStorage.getItem("deaths15")]   
-      //   ] 
-      // },
-      // {
-      //   label: 'Positives',
-      //   data: 
-      //   [
-      //     [sessionStorage.getItem("dates1"),  sessionStorage.getItem("positives1") - sessionStorage.getItem("positives2")],   
-      //     [sessionStorage.getItem("dates2"),  sessionStorage.getItem("positives2") - sessionStorage.getItem("positives3")],   
-      //     [sessionStorage.getItem("dates3"),  sessionStorage.getItem("positives3") - sessionStorage.getItem("positives4")],   
-      //     [sessionStorage.getItem("dates4"),  sessionStorage.getItem("positives4") - sessionStorage.getItem("positives5")],   
-      //     [sessionStorage.getItem("dates5"),  sessionStorage.getItem("positives5") - sessionStorage.getItem("positives6")],   
-      //     [sessionStorage.getItem("dates6"),  sessionStorage.getItem("positives6") - sessionStorage.getItem("positives7")],   
-      //     [sessionStorage.getItem("dates7"),  sessionStorage.getItem("positives7") - sessionStorage.getItem("positives8")],   
-      //     [sessionStorage.getItem("dates8"),  sessionStorage.getItem("positives8") - sessionStorage.getItem("positives9")],   
-      //     [sessionStorage.getItem("dates9"),  sessionStorage.getItem("positives9") - sessionStorage.getItem("positives10")],   
-      //     [sessionStorage.getItem("dates10"),  sessionStorage.getItem("positives10") - sessionStorage.getItem("positives11")],   
-      //     [sessionStorage.getItem("dates11"),  sessionStorage.getItem("positives11") - sessionStorage.getItem("positives12")],   
-      //     [sessionStorage.getItem("dates12"),  sessionStorage.getItem("positives12") - sessionStorage.getItem("positives13")],   
-      //     [sessionStorage.getItem("dates13"),  sessionStorage.getItem("positives13") - sessionStorage.getItem("positives14")],   
-      //     [sessionStorage.getItem("dates14"),  sessionStorage.getItem("positives14") - sessionStorage.getItem("positives15")]   
-      //   ] 
-      // }]
     }))
-
-  // React.useEffect(updateChartData)
 
   return {...state, updateChartData}
 
@@ -671,13 +518,7 @@ function App() {
 
   const axes = React.useMemo(() => [{ primary: true, type: 'linear', position: 'bottom' }, { type: 'linear', position: 'left' }],[])
 
-  const { data, updateChartData } = useChartConfig() // gets called first and calls getChartData
-
-  // useEffect(()=> {
-  //   updateChartData()
-  // },[])
-
-  // initialize()
+  const { data, updateChartData } = useChartConfig()
 
   let lineChart = (
     <div style={{ margin: 'auto', width: '80vw', height: '80vh', maxWidth: '-webkit-fill-available', maxHeight: '-webkit-fill-available' }}>
@@ -689,7 +530,6 @@ function App() {
   <div className="App" >
       <header className="App-header">
       
-
         {/* STATE SEARCH */}
         <h3 hidden="true" id="loading">Loading . . .</h3>
         <Form id="formStateInput" className="state-form" onSubmit={e => {get_data(); e.preventDefault();}}>
@@ -701,7 +541,6 @@ function App() {
             </div>
           </Form.Group>
         </Form>
-
 
         <Card id="root" className="card" border="secondary" bg="light" text="dark">
           <Card.Body>
