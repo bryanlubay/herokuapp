47a747a715c075e2fd4692a2519ff7d717bf7441
Bryan Lubay <BLubay@ArsenalInc.local>
2020-12-23 09:23:08 -0800

    23 Dec Ars

--- src/App.js
+++ src/App.js
@@ -556,7 +556,7 @@ function App() {
         </Card>
 
         <div className="card-chart">
-          <p id="chart-header" className="x-axis">Days VS Infected <span style="color: red">Positives</span>  <span style="color: blue">Deaths</span> ( Work in progress :D )</p>
+          <p id="chart-header" className="x-axis">Days VS Infected <span color="red">Positives</span>  <span color="color: blue">Deaths</span> ( Work in progress :D )</p>
           {lineChart}
         </div>
 
