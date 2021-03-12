15a8dfdcf8d72ee858a6e0cf8bf9374719fb0237
Bryan Lubay <BLubay@ArsenalInc.local>
2021-03-12 12:58:59 -0800

    12 March Ars

--- src/components/Coronavirus.js
+++ src/components/Coronavirus.js
@@ -296,6 +296,118 @@ function convertEpoch(epoch) {
 }
 
 
+const get_data = async () => {
+
+  sessionStorage.clear()
+  let state = convertState(document.getElementById('input').value) // move/change this
+  document.getElementById("formStateInput").hidden = true
+  document.getElementById("loading").hidden = false
+
+  let res = await fetch('https://bryanlubayapi.herokuapp.com/get_data/' + state + '/', {
+    method: 'GET',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    },
+    params: {
+      'state': state
+    }
+  })
+
+  let data = await res.json()
+  sessionStorage.setItem("dates1", convertEpoch(data.Date[data.Date.length - 1])) // newest update
+  sessionStorage.setItem("dates2", convertEpoch(data.Date[data.Date.length - 2])) 
+  sessionStorage.setItem("dates3", convertEpoch(data.Date[data.Date.length - 3])) 
+  sessionStorage.setItem("dates4", convertEpoch(data.Date[data.Date.length - 4])) 
+  sessionStorage.setItem("dates5", convertEpoch(data.Date[data.Date.length - 5])) 
+  sessionStorage.setItem("dates6", convertEpoch(data.Date[data.Date.length - 6])) 
+  sessionStorage.setItem("dates7", convertEpoch(data.Date[data.Date.length - 7])) 
+  sessionStorage.setItem("dates8", convertEpoch(data.Date[data.Date.length - 8])) 
+  sessionStorage.setItem("dates9", convertEpoch(data.Date[data.Date.length - 9])) 
+  sessionStorage.setItem("dates10", convertEpoch(data.Date[data.Date.length - 10])) 
+  sessionStorage.setItem("dates11", convertEpoch(data.Date[data.Date.length - 11])) 
+  sessionStorage.setItem("dates12", convertEpoch(data.Date[data.Date.length - 12])) 
+  sessionStorage.setItem("dates13", convertEpoch(data.Date[data.Date.length - 13])) 
+  sessionStorage.setItem("dates14", convertEpoch(data.Date[data.Date.length - 14])) 
+  sessionStorage.setItem("dates15", convertEpoch(data.Date[data.Date.length - 15])) 
+
+  sessionStorage.setItem("positives1", parseInt(data.Positive[data.Positive.length - 1]))
+  sessionStorage.setItem("positives2", parseInt(data.Positive[data.Positive.length - 2]))
+  sessionStorage.setItem("positives3", parseInt(data.Positive[data.Positive.length - 3]))
+  sessionStorage.setItem("positives4", parseInt(data.Positive[data.Positive.length - 4]))
+  sessionStorage.setItem("positives5", parseInt(data.Positive[data.Positive.length - 5]))
+  sessionStorage.setItem("positives6", parseInt(data.Positive[data.Positive.length - 6]))
+  sessionStorage.setItem("positives7", parseInt(data.Positive[data.Positive.length - 7]))
+  sessionStorage.setItem("positives8", parseInt(data.Positive[data.Positive.length - 8]))
+  sessionStorage.setItem("positives9", parseInt(data.Positive[data.Positive.length - 9]))
+  sessionStorage.setItem("positives10", parseInt(data.Positive[data.Positive.length - 10]))
+  sessionStorage.setItem("positives11", parseInt(data.Positive[data.Positive.length - 11]))
+  sessionStorage.setItem("positives12", parseInt(data.Positive[data.Positive.length - 12]))
+  sessionStorage.setItem("positives13", parseInt(data.Positive[data.Positive.length - 13]))
+  sessionStorage.setItem("positives14", parseInt(data.Positive[data.Positive.length - 14]))
+  sessionStorage.setItem("positives15", parseInt(data.Positive[data.Positive.length - 15]))
+
+  sessionStorage.setItem("deaths1", parseInt(data.Deaths[data.Deaths.length - 1]))
+  sessionStorage.setItem("deaths2", parseInt(data.Deaths[data.Deaths.length - 2]))
+  sessionStorage.setItem("deaths3", parseInt(data.Deaths[data.Deaths.length - 3]))
+  sessionStorage.setItem("deaths4", parseInt(data.Deaths[data.Deaths.length - 4]))
+  sessionStorage.setItem("deaths5", parseInt(data.Deaths[data.Deaths.length - 5]))
+  sessionStorage.setItem("deaths6", parseInt(data.Deaths[data.Deaths.length - 6]))
+  sessionStorage.setItem("deaths7", parseInt(data.Deaths[data.Deaths.length - 7]))
+  sessionStorage.setItem("deaths8", parseInt(data.Deaths[data.Deaths.length - 8]))
+  sessionStorage.setItem("deaths9", parseInt(data.Deaths[data.Deaths.length - 9]))
+  sessionStorage.setItem("deaths10", parseInt(data.Deaths[data.Deaths.length - 10]))
+  sessionStorage.setItem("deaths11", parseInt(data.Deaths[data.Deaths.length - 11]))
+  sessionStorage.setItem("deaths12", parseInt(data.Deaths[data.Deaths.length - 12]))
+  sessionStorage.setItem("deaths13", parseInt(data.Deaths[data.Deaths.length - 13]))
+  sessionStorage.setItem("deaths14", parseInt(data.Deaths[data.Deaths.length - 14]))
+  sessionStorage.setItem("deaths15", parseInt(data.Deaths[data.Deaths.length - 15]))
+
+
+  let asterik = "*"
+  convertState(state)
+  var number = parseInt(document.getElementById('tested').textContent = data.Tested[data.Tested.length - 1]) - parseInt(document.getElementById('tested').textContent = data.Tested[data.Tested.length - 2])
+
+  document.getElementById('tested').textContent = parseInt(data.Tested[data.Tested.length - 1]).toLocaleString() + " Tested (+" + number.toLocaleString() + ")" + asterik
+
+  number = parseInt(document.getElementById('cases').textContent = data.Positive[data.Positive.length - 1]) - parseInt(document.getElementById('cases').textContent = data.Positive[data.Positive.length - 2])
+  document.getElementById('cases').textContent = parseInt(data.Positive[data.Positive.length - 1]).toLocaleString() + " Positive Cases (+" + number.toLocaleString() + ")" + asterik
+
+  number = parseInt(document.getElementById('deaths').textContent = data.Deaths[data.Deaths.length - 1]) - parseInt(document.getElementById('deaths').textContent = data.Deaths[data.Deaths.length - 2])
+  document.getElementById('deaths').textContent = parseInt(data.Deaths[data.Deaths.length - 1]).toLocaleString() + " Deaths (+" + number.toLocaleString() + ")" + asterik
+
+  var d = new Date(0);
+  d.setUTCSeconds(data.Date[data.Date.length - 1])
+
+  document.getElementById('last_updated').textContent = "Last Updated: " + d.toUTCString()
+
+  d = new Date(0);
+  d.setUTCSeconds(data.Date[data.Date.length - 2])
+
+  document.getElementById('update_before_last').textContent = asterik + "Update Before Last: " + d.toUTCString()
+
+  document.getElementById("loading").hidden = true
+  document.getElementById("formStateInput").hidden = false
+
+  updateChartData()
+
+  return data
+}; // End get_data
+
+const [state, setState] = React.useState(
+  { 
+  data: [
+    {
+      label: 'Deaths',
+      data: [[10000,10000]]
+    },
+    {
+      label: 'New Positive Cases',
+      data: [[10000,10000]]
+    }]})
+
+
 /************************************************************************/
 /************************************************************************/
 /************************************************************************/
@@ -347,116 +459,6 @@ function update_chart () {
 
 const Coronavirus = () => {
 
-  const get_data = async () => {
-
-    sessionStorage.clear()
-    let state = convertState(document.getElementById('input').value) // move/change this
-    document.getElementById("formStateInput").hidden = true
-    document.getElementById("loading").hidden = false
-  
-    let res = await fetch('https://bryanlubayapi.herokuapp.com/get_data/' + state + '/', {
-      method: 'GET',
-      mode: 'cors',
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-      },
-      params: {
-        'state': state
-      }
-    })
-  
-    let data = await res.json()
-    sessionStorage.setItem("dates1", convertEpoch(data.Date[data.Date.length - 1])) // newest update
-    sessionStorage.setItem("dates2", convertEpoch(data.Date[data.Date.length - 2])) 
-    sessionStorage.setItem("dates3", convertEpoch(data.Date[data.Date.length - 3])) 
-    sessionStorage.setItem("dates4", convertEpoch(data.Date[data.Date.length - 4])) 
-    sessionStorage.setItem("dates5", convertEpoch(data.Date[data.Date.length - 5])) 
-    sessionStorage.setItem("dates6", convertEpoch(data.Date[data.Date.length - 6])) 
-    sessionStorage.setItem("dates7", convertEpoch(data.Date[data.Date.length - 7])) 
-    sessionStorage.setItem("dates8", convertEpoch(data.Date[data.Date.length - 8])) 
-    sessionStorage.setItem("dates9", convertEpoch(data.Date[data.Date.length - 9])) 
-    sessionStorage.setItem("dates10", convertEpoch(data.Date[data.Date.length - 10])) 
-    sessionStorage.setItem("dates11", convertEpoch(data.Date[data.Date.length - 11])) 
-    sessionStorage.setItem("dates12", convertEpoch(data.Date[data.Date.length - 12])) 
-    sessionStorage.setItem("dates13", convertEpoch(data.Date[data.Date.length - 13])) 
-    sessionStorage.setItem("dates14", convertEpoch(data.Date[data.Date.length - 14])) 
-    sessionStorage.setItem("dates15", convertEpoch(data.Date[data.Date.length - 15])) 
-  
-    sessionStorage.setItem("positives1", parseInt(data.Positive[data.Positive.length - 1]))
-    sessionStorage.setItem("positives2", parseInt(data.Positive[data.Positive.length - 2]))
-    sessionStorage.setItem("positives3", parseInt(data.Positive[data.Positive.length - 3]))
-    sessionStorage.setItem("positives4", parseInt(data.Positive[data.Positive.length - 4]))
-    sessionStorage.setItem("positives5", parseInt(data.Positive[data.Positive.length - 5]))
-    sessionStorage.setItem("positives6", parseInt(data.Positive[data.Positive.length - 6]))
-    sessionStorage.setItem("positives7", parseInt(data.Positive[data.Positive.length - 7]))
-    sessionStorage.setItem("positives8", parseInt(data.Positive[data.Positive.length - 8]))
-    sessionStorage.setItem("positives9", parseInt(data.Positive[data.Positive.length - 9]))
-    sessionStorage.setItem("positives10", parseInt(data.Positive[data.Positive.length - 10]))
-    sessionStorage.setItem("positives11", parseInt(data.Positive[data.Positive.length - 11]))
-    sessionStorage.setItem("positives12", parseInt(data.Positive[data.Positive.length - 12]))
-    sessionStorage.setItem("positives13", parseInt(data.Positive[data.Positive.length - 13]))
-    sessionStorage.setItem("positives14", parseInt(data.Positive[data.Positive.length - 14]))
-    sessionStorage.setItem("positives15", parseInt(data.Positive[data.Positive.length - 15]))
-  
-    sessionStorage.setItem("deaths1", parseInt(data.Deaths[data.Deaths.length - 1]))
-    sessionStorage.setItem("deaths2", parseInt(data.Deaths[data.Deaths.length - 2]))
-    sessionStorage.setItem("deaths3", parseInt(data.Deaths[data.Deaths.length - 3]))
-    sessionStorage.setItem("deaths4", parseInt(data.Deaths[data.Deaths.length - 4]))
-    sessionStorage.setItem("deaths5", parseInt(data.Deaths[data.Deaths.length - 5]))
-    sessionStorage.setItem("deaths6", parseInt(data.Deaths[data.Deaths.length - 6]))
-    sessionStorage.setItem("deaths7", parseInt(data.Deaths[data.Deaths.length - 7]))
-    sessionStorage.setItem("deaths8", parseInt(data.Deaths[data.Deaths.length - 8]))
-    sessionStorage.setItem("deaths9", parseInt(data.Deaths[data.Deaths.length - 9]))
-    sessionStorage.setItem("deaths10", parseInt(data.Deaths[data.Deaths.length - 10]))
-    sessionStorage.setItem("deaths11", parseInt(data.Deaths[data.Deaths.length - 11]))
-    sessionStorage.setItem("deaths12", parseInt(data.Deaths[data.Deaths.length - 12]))
-    sessionStorage.setItem("deaths13", parseInt(data.Deaths[data.Deaths.length - 13]))
-    sessionStorage.setItem("deaths14", parseInt(data.Deaths[data.Deaths.length - 14]))
-    sessionStorage.setItem("deaths15", parseInt(data.Deaths[data.Deaths.length - 15]))
-
-
-    let asterik = "*"
-    convertState(state)
-    var number = parseInt(document.getElementById('tested').textContent = data.Tested[data.Tested.length - 1]) - parseInt(document.getElementById('tested').textContent = data.Tested[data.Tested.length - 2])
-
-    document.getElementById('tested').textContent = parseInt(data.Tested[data.Tested.length - 1]).toLocaleString() + " Tested (+" + number.toLocaleString() + ")" + asterik
-  
-    number = parseInt(document.getElementById('cases').textContent = data.Positive[data.Positive.length - 1]) - parseInt(document.getElementById('cases').textContent = data.Positive[data.Positive.length - 2])
-    document.getElementById('cases').textContent = parseInt(data.Positive[data.Positive.length - 1]).toLocaleString() + " Positive Cases (+" + number.toLocaleString() + ")" + asterik
-  
-    number = parseInt(document.getElementById('deaths').textContent = data.Deaths[data.Deaths.length - 1]) - parseInt(document.getElementById('deaths').textContent = data.Deaths[data.Deaths.length - 2])
-    document.getElementById('deaths').textContent = parseInt(data.Deaths[data.Deaths.length - 1]).toLocaleString() + " Deaths (+" + number.toLocaleString() + ")" + asterik
-  
-    var d = new Date(0);
-    d.setUTCSeconds(data.Date[data.Date.length - 1])
-  
-    document.getElementById('last_updated').textContent = "Last Updated: " + d.toUTCString()
-  
-    d = new Date(0);
-    d.setUTCSeconds(data.Date[data.Date.length - 2])
-  
-    document.getElementById('update_before_last').textContent = asterik + "Update Before Last: " + d.toUTCString()
-  
-    document.getElementById("loading").hidden = true
-    document.getElementById("formStateInput").hidden = false
-  
-    updateChartData()
-
-    return data
-  }; // End get_data
-  
-  const [state, setState] = React.useState(
-    { 
-    data: [
-      {
-        label: 'Deaths',
-        data: [[10000,10000]]
-      },
-      {
-        label: 'New Positive Cases',
-        data: [[10000,10000]]
-      }]})
 
   function useChartConfig() { 
 
