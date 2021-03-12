a271c8f8725e1001998ad50b017761b48a1970dd
Bryan Lubay <BLubay@ArsenalInc.local>
2021-03-12 12:51:19 -0800

    12 March Ars

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@
 import React from 'react';
 import { Card, Form, Button, Alert, Accordion, Table, Image } from 'react-bootstrap';
 import { BrowserRouter as Router, Route, /*Redirect,*/ Switch } from 'react-router-dom';
-import { Chart } from 'react-charts'
 import Container from 'react-bootstrap/Container'
 import Unity from './components/Unity'
 import Coronavirus from './components/Coronavirus'
@@ -34,12 +33,10 @@ function App() {
         <Navbar fixed="top"  bg="light" expand="lg">
                 <Navbar.Brand>Bryan Lubay's App</Navbar.Brand>
                 <Nav.Link href="/comptia">CompTIA</Nav.Link>
-                {/* <Nav.Link href="/coronavirus">Coronavirus</Nav.Link> */}
+                <Nav.Link href="/coronavirus">Coronavirus</Nav.Link>
                 <Nav.Link href="/unity">Unity</Nav.Link>
         </Navbar>
       </Container>
-
-
 
 
 
@@ -49,7 +46,7 @@ function App() {
         <Switch>
           <Route exact path='/unity' render={Unity}/> 
           <Route exact path='/comptia' render={CompTIA}/> 
-          {/* <Route exact path='/coronavirus' render={Coronavirus}/>  */}
+          <Route exact path='/coronavirus' render={Coronavirus}/> 
 
         </Switch>
       </Router>
