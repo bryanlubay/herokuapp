1e8fa2ce79113dcdf57bb9ef40f6cdbb51fa730e
Bryan Lubay <BLubay@ArsenalInc.local>
2021-03-12 13:15:26 -0800

    12 March Ars

--- src/components/Coronavirus.js
+++ src/components/Coronavirus.js
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import { Card, Form, Button, Alert, Accordion, Table, Image } from 'react-bootstrap';
 import { Chart } from 'react-charts'
 
