41b2445fb581e3aab75069f87635c964b6a1590d
Bryan Lubay <BLubay@ArsenalInc.local>
2021-03-15 08:31:47 -0700

    15 March Ars

--- src/components/CompTIA.js
+++ src/components/CompTIA.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import ReactCardFlip from 'react-card-flip';
 
 const CompTIA = () => {
   return (
     <div>
-      <h1>CompTIA flash cards app coming soon :)</h1>
+      {/* <h1>CompTIA flash cards app coming soon :)</h1> */}
+
+      <ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="vertical">
+        <YOUR_FRONT_CCOMPONENT>
+          This is the front of the card.
+          <button onClick={this.handleClick}>Click to flip</button>
+        </YOUR_FRONT_CCOMPONENT>
+
+        <YOUR_BACK_COMPONENT>
+          This is the back of the card.
+          <button onClick={this.handleClick}>Click to flip</button>
+        </YOUR_BACK_COMPONENT>
+      </ReactCardFlip>
+
+
     </div>
   )
 }
