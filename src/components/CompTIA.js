93300699156cf44c2d568808ee23ab0c6eaca7df
Bryan Lubay <BLubay@ArsenalInc.local>
2021-03-15 10:39:48 -0700

    15 March Ars

--- src/components/CompTIA.js
+++ src/components/CompTIA.js
@@ -1,21 +1,45 @@
 import React from 'react';
-import {Card} from 'react-bootstrap';
+import {Button, Card} from 'react-bootstrap';
 
 import './CompTIA.css'
 
+function show_answer() {
+  document.getElementById("back").hidden = false;
+  document.getElementById("show_button").hidden = true;
+  document.getElementById("hide_button").hidden = false;
+}
+
+function hide_answer() {
+  document.getElementById("back").hidden = true;
+  document.getElementById("show_button").hidden = false;
+  document.getElementById("hide_button").hidden = true;
+}
+
+
 const CompTIA = () => {
 
+  document.getElementById("back").hidden = true;
+  document.getElementById("hide_button").hidden = true;
 
   return (
     <div>
 
     <Card>
-      <p className="front">
+      <p id="front" className="front">
         Front
       </p>
-      <p className="back">
+      <p id="back" className="back">
         Back
       </p>
+
+      <Button id="show_button" onClick={() => show_answer()}>
+        Show Answer
+      </Button>
+
+      <Button id="hide_button" onClick={() => hide_answer()}>
+        Hide Answer
+      </Button>
+
     </Card>
 
 
