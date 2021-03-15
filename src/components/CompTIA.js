684be24d158f0cbc720ab447249448aed49d4cbf
Bryan Lubay <BLubay@ArsenalInc.local>
2021-03-15 09:58:57 -0700

    15 March Ars

--- src/components/CompTIA.js
+++ src/components/CompTIA.js
@@ -1,72 +1,20 @@
 import React from 'react';
-import ReactCardFlip from 'react-card-flip';
-
-class App extends React.Component {
-  constructor() {
-    super();
-      this.state = {
-      isFlipped: false
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick(e) {
-    e.preventDefault();
-    this.setState(prevState => ({ isFlipped: !prevState.isFlipped }));
-  }
-}
-
-
-// function handleClick(e) {
-//   e.preventDefault();
-//   this.setState(prevState => ({ isFlipped: !prevState.isFlipped }));
-// }
-
+import Card from 'react-bootstrap'
 
 const CompTIA = () => {
 
-  // constructor(); {
-  //   super();
-  //     this.state = {
-  //     isFlipped: false
-  //   };
-  //   this.handleClick = this.handleClick.bind(this);
-  // }
-
-  // handleClick(e) {
-  //   e.preventDefault();
-  //   this.setState(prevState => ({ isFlipped: !prevState.isFlipped }));
-  // }
-  // this.state = {
-  //   isFlipped: false
-  // };
-
-
-  // const state  = React.setState(
-  //   { 
-  //     isFlipped: false
-  //   })
-
-  // this.state = {
-  //   isFlipped: false
-  // };
 
   return (
     <div>
-      {/* <h1>CompTIA flash cards app coming soon :)</h1> */}
 
-      {/* <ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="vertical"> */}
-      <ReactCardFlip>
-        <p>
-          This is the front of the card.
-          <button onClick={this.handleClick}>Click to flip</button>
-        </p>
-
-        <p>
-          This is the back of the card.
-          <button onClick={this.handleClick}>Click to flip</button>
-        </p>
-      </ReactCardFlip>
+    <Card>
+      <p>
+        Front
+      </p>
+      <p>
+        Back
+      </p>
+    </Card>
 
 
     </div>
